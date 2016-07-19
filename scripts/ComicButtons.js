a34d5f8489c31ee2fa3a38d9372e59c66b6381b4
PixelPiledriver <pixelpiledriver@gmail.com>
2016-07-18 19:49:01 -0700

    test komposer site
    Been coding site in Komposer.
    Testing to see if it will work on internet.

---
+++ scripts/ComicButtons.js
@@ -0,0 +1,127 @@
+function nuttinButton1()
+{
+	document.getElementById("nuttinComic").src = "images/nuttin_panel1.png"
+}
+
+function nuttinButton2()
+{
+	document.getElementById("nuttinComic").src = "images/nuttin_panel2.png"
+}
+
+function nuttinButton3()
+{
+	document.getElementById("nuttinComic").src = "images/nuttin_panel3.png"
+}
+
+
+function MonkeyTimeButton1()
+{
+	document.getElementById("MonkeyTime").src = "images/MonkeyTime_panel1.gif"
+}
+
+function MonkeyTimeButton2()
+{
+	document.getElementById("MonkeyTime").src = "images/MonkeyTime_panel2.gif"
+}
+
+function MonkeyTimeButton3()
+{
+	document.getElementById("MonkeyTime").src = "images/MonkeyTime_panel3.gif"
+}
+
+function MonkeyTimeButton4()
+{
+	document.getElementById("MonkeyTime").src = "images/MonkeyTime_panel4.gif"
+}
+
+
+function OverGuy()
+{
+	document.getElementById("Guy").src = "images/Portrait_Guy.png"
+}
+function OutGuy()
+{
+	document.getElementById("Guy").src = "images/Portrait_Blank.png"
+}
+
+function OverYeti()
+{
+	document.getElementById("Yeti").src = "images/Portrait_Yeti.png"
+}
+function OutYeti()
+{
+	document.getElementById("Yeti").src = "images/Portrait_Blank.png"
+}
+
+function OverCat()
+{
+	document.getElementById("Cat").src = "images/Portrait_Cat.png"
+}
+function OutCat()
+{
+	document.getElementById("Cat").src = "images/Portrait_Blank.png"
+}
+
+function OverDood()
+{
+	document.getElementById("Dood").src = "images/Portrait_Dood.png"
+}
+function OutDood()
+{
+	document.getElementById("Dood").src = "images/Portrait_Blank.png"
+}
+
+function OverPizza()
+{
+	document.getElementById("Pizza").src = "images/Portrait_Pizza.png"
+}
+function OutPizza()
+{
+	document.getElementById("Pizza").src = "images/Portrait_Blank.png"
+}
+
+function OverGirl()
+{
+	document.getElementById("Girl").src = "images/Portrait_Girl.png"
+}
+function OutGirl()
+{
+	document.getElementById("Girl").src = "images/Portrait_Blank.png"
+}
+
+function OverDick()
+{
+	document.getElementById("Dick").src = "images/Portrait_Dick.gif"
+}
+function OutDick()
+{
+	document.getElementById("Dick").src = "images/Portrait_Blank.png"
+}
+
+function OverBlood()
+{
+	document.getElementById("Blood").src = "images/Portrait_Blood.png"
+}
+function OutBlood()
+{
+	document.getElementById("Blood").src = "images/Portrait_Blank.png"
+}
+
+function OverBox()
+{
+	document.getElementById("Box").src = "images/Portrait_Box.png"
+}
+function OutBox()
+{
+	document.getElementById("Box").src = "images/Portrait_Blank.png"
+}
+
+function OverGirlClothes()
+{
+	document.getElementById("GirlClothes").src = "images/GirlClothes_Off.png"
+}
+function OutGirlClothes()
+{
+	document.getElementById("GirlClothes").src = "images/GirlClothes_On.png"
+}
+
